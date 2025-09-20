8569c8cbca5d3d78c2f27e85679c53365f0e029f
Konstantin Voykov <koykov0@gmail.com>
2025-09-20 23:07:42 +0300

    conn: follow interface

--- conn.go
+++ conn.go
@@ -1,15 +1,51 @@
 package mmdbc
 
-import "net/netip"
+import (
+	"io"
+	"net/netip"
+	"os"
+)
 
 type Connection interface {
 	Get(ip netip.Addr) (*Tuple, error)
 	Gets(ip string) (*Tuple, error)
 	PGet(dst *Tuple, ip netip.Addr) error
 	PGets(dst *Tuple, ip string) error
+	io.Closer
 }
 
-func Connect(filePath string) (Connection, error) {
+func Connect(filePath string) (_ Connection, err error) {
+	var c conn
+	if c.f, err = os.Open(filePath); err != nil {
+		return
+	}
+	return &c, nil
+}
+
+type conn struct {
+	f *os.File
+}
+
+func (c *conn) Get(ip netip.Addr) (*Tuple, error) {
 	// todo implement me
 	return nil, nil
+}
+
+func (c *conn) Gets(ip string) (*Tuple, error) {
+	// todo implement me
+	return nil, nil
+}
+
+func (c *conn) PGet(dst *Tuple, ip netip.Addr) error {
+	// todo implement me
+	return nil
+}
+
+func (c *conn) PGets(dst *Tuple, ip string) error {
+	// todo implement me
+	return nil
+}
+
+func (c *conn) Close() error {
+	return c.f.Close()
 }
