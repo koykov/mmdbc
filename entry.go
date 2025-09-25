114db9947191617cdb763acaeb99452ce1d67896
Konstantin Voykov <koykov0@gmail.com>
2025-09-25 22:45:05 +0300

    declare entry

---
+++ entry.go
@@ -0,0 +1,31 @@
+package mmdbc
+
+type entryType uint8
+
+const (
+	entryExtended entryType = iota
+	entryPointer
+	entryString
+	entryDouble
+	entryBytes
+	entryUint16
+	entryUint32
+	entryMap
+	entryInt32
+	entryUint64
+	entryUint128
+	entryArray
+	entryContainer
+	entryEndMarker
+	entryBool
+	entryFloat
+)
+
+type entry struct {
+	hasData      bool
+	data         [16]byte
+	offset       uint32
+	offsetToNext uint32
+	dataSize     uint32
+	typ          uint32
+}
