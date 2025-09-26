a97dca50c41423a55445c922a76d5a11573078e2
Konstantin Voykov <koykov0@gmail.com>
2025-09-26 23:24:41 +0300

    decoder: uint16

---
+++ decoder.go
@@ -0,0 +1,15 @@
+package mmdbc
+
+import "io"
+
+func decodeUint16(buf []byte, offset, size uint64) (uint16, uint64, error) {
+	if offset+size > uint64(len(buf)) {
+		return 0, 0, io.ErrUnexpectedEOF
+	}
+	b := buf[offset : offset+size]
+	var r uint16
+	for i := 0; i < len(b); i++ {
+		r = (r << 8) | uint16(b[i])
+	}
+	return r, offset + size, nil
+}
