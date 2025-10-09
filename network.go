a4be5f215041367f8d9695265d7145d02d2e76d7
Konstantin Voykov <koykov0@gmail.com>
2025-10-09 23:41:36 +0300

    network: planning walker

--- network.go
+++ network.go
@@ -31,19 +31,37 @@ func (c *conn) EachNetworkWithOptions(ctx context.Context, fn func(*Tuple) error
 		return ErrOverflowPrefix
 	}
 
-	addr, bits := pfx.Addr(), pfx.Bits()
-	if addr.Is4() {
-		raw := addr.As4()
+	addrRaw, bits := pfx.Addr(), pfx.Bits()
+	addr := addrRaw
+	if addrRaw.Is4() {
+		raw := addrRaw.As4()
 		var raw6 [16]byte
 		copy(raw6[12:], raw[:])
-		addr = netip.AddrFrom16(raw6)
+		addrRaw = netip.AddrFrom16(raw6)
 		bits += 96
 	}
 	if bits > 128 {
 		return ErrOverflowIPv6
 	}
 
-	return c.eachNetwork(ctx, &addr, pfx.Bits(), fn, options)
+	root, bit, err := c.traverse(ctx, &addrRaw, 0, bits)
+	if err != nil {
+		return err
+	}
+	if pfx, err = addr.Prefix(int(bit)); err != nil {
+		return err
+	}
+
+	if err = c.netwalk(ctx, root, fn, options, 0); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *conn) netwalk(ctx context.Context, root uint64, fn func(*Tuple) error, options NetworkOption, depth int) error {
+	// todo implement me
+	return nil
 }
 
 func (c *conn) eachNetwork(ctx context.Context, addr *netip.Addr, bits int, fn func(*Tuple) error, options NetworkOption) error {
@@ -60,3 +78,5 @@ var allpfx = [7]netip.Prefix{
 	{},
 	netip.MustParsePrefix("::/0"),
 }
+
+var _ = NetworkOptionAll
